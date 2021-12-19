b4ad95ba62c2d82d75140ccf01a01aa5bab2db82
Kristina <ktuzova1901@gmail.com>
2021-12-19 02:51:55 +0200

    add js

--- task-01.js
+++ task-01.js
@@ -1,30 +1,38 @@
-// Задача: необхідно створити форму в якій буде 2 інпута для чисел і один селект для математичної операції(+, -,*, /). 
-// По сабміту форми користувач має побачити результат.Результат має бути завжди цілим числом.Додай лейбли до полів вводу та анотації, щоб було інтуітивно зрозуміло що робити. 
+// Задача: необхідно створити форму в якій буде 2 інпута для чисел і один селект для математичної операції(+, -,*, /).
+// По сабміту форми користувач має побачити результат.Результат має бути завжди цілим числом.Додай лейбли до полів вводу та анотації, щоб було інтуітивно зрозуміло що робити.
 // Завдання можеш оформити у Github та прислати нам посиланням на репозіторій або розшарити нам його за допомогою гуглдоку.
 
-let numberFirst = document.getElementById("numberf").value;
-let numberSecond = document.getElementById("numberc").value;
+publish.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { numberf, numberc }
+  } = event.currentTarget;
+
+  if  (numberf.value === "" || numberc.value === "") {
+    return alert("Будь ласка, введіть число!");
+  }
+let numberFirst = document.getElementById("first_value").value;
+let numberSecond = document.getElementById("second_value").value;
 let numberFirstValue = parseFloat(numberFirst);
 let numberSecondValue = parseFloat(numberSecond);
-let result = document.getElementById("result_multi").textContent;
-let result_Number = parseFloat(result);
-const button = document.querySelector(".my-button");
-let action  = document.getElementById('sample').value;
-console.log(numberFirstValue)
-
-   switch (action) {
-   case "+":
-        result = numberFirstValue + numberSecondValue;
-        console.log(result);
+let action = document.getElementById('sample').value;
+let multi = 0;
+switch (action) {
+    case "+":
+      multi = numberFirstValue + numberSecondValue;
       break
-   case "-":
-     result = numberFirstValue - numberSecondValue;
-      break   
+    case "-":
+      multi = numberFirstValue - numberSecondValue;
+      break
     case "*":
-     result = numberFirstValue * numberSecondValue;
-      break   
+      multi = numberFirstValue * numberSecondValue;
+     break
     case "/":
-     result = numberFirstValue / numberSecondValue;
-      break   
-    }
-    
+      multi = numberFirstValue / numberSecondValue;
+      break
+  }
+  let result = document.getElementById("result_multi");
+ result.innerHTML = Math.round(multi); 
+}
